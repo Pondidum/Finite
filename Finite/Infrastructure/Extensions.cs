104da6c0da1f8364349c9f679de02ec2398ea9cc
andy <andy@andydote.co.uk>
2015-03-15 22:57:27 +0000

    extensions: add extension methods

---
+++ Finite/Infrastructure/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Finite.Infrastructure
+{
+	public static class Extensions
+	{
+		public static void ForEach<T>(this IEnumerable<T> self, Action<T> action)
+		{
+			foreach (var item in self)
+			{
+				action(item);
+			}
+		}
+
+		public static void AddRange<TKey, TValue>(this IDictionary<TKey, TValue> self, IEnumerable<KeyValuePair<TKey, TValue>> items)
+		{
+			items.ForEach(self.Add);
+		}
+
+	}
+}
