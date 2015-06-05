15732066b99e8d7b32d2e50d58f0c07eb4387c9a
andy <andy@andydote.co.uk>
2015-06-05 23:35:45 +0100

    machineConfiguration: no need for instance configuration interface here

--- Finite/Configurations/MachineConfiguration.cs
+++ Finite/Configurations/MachineConfiguration.cs
@@ -2,19 +2,11 @@
 {
 	public class MachineConfiguration<T>
 	{
-		public IInstanceCreator InstanceCreator { get; private set; }
 		public IStateChangedHandler<T> StateChangedHandler { get; private set; }
 
 		public MachineConfiguration()
 		{
-			CreateInstancesWith(new DefaultInstanceCreator());
 			OnStateChange(new DefaultStateChangedHandler<T>());
-		}
-
-		public MachineConfiguration<T> CreateInstancesWith(IInstanceCreator instanceCreator)
-		{
-			InstanceCreator = instanceCreator;
-			return this;
 		}
 
 		public MachineConfiguration<T> OnStateChange(IStateChangedHandler<T> handler)
