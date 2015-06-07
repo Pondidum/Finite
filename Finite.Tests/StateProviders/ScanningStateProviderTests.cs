d552c3f1b298d0ff3f9d972220e8abf5a0483a2b
andy <andy@andydote.co.uk>
2015-06-07 16:36:38 +0100

    tests: rename test to match class under test

--- Finite.Tests/StateProviders/ScanningStateProviderTests.cs
+++ Finite.Tests/StateProviders/ScanningStateProviderTests.cs
@@ -4,16 +4,21 @@ using Finite.Tests.Acceptance.States;
 using Shouldly;
 using Xunit;
 
-namespace Finite.Tests
+namespace Finite.Tests.StateProviders
 {
-	public class StatesExtensionsTests
+	public class ScanningStateProviderTests
 	{
 		[Fact]
 		public void Scan_should_find_all_relevant_state_types()
 		{
 			var states = new ScanningStateProvider<LightsSwitches>();
 
-			states.KnownTypes.ShouldBe(new[] {typeof(LightOff), typeof(LightOnFull), typeof(LightOnDim) }, true);
+			states.KnownTypes.ShouldBe(new[]
+			{
+				typeof(LightOff), 
+				typeof(LightOnFull), 
+				typeof(LightOnDim)
+			}, true);
 		}
 	}
 }
