8b80d4142592df1a6c72f57e5e9cf7af83ed1a74
andy <andy@andydote.co.uk>
2015-10-02 18:21:46 +0100

    sample.common: add creditRequest model

---
+++ Sample.Common/CreditRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Sample.Common
+{
+	public class CreditRequest
+	{
+		public decimal Amount { get; set; }
+		public string Justification { get; set; }
+
+		public DateTime CreatedOn { get; set; }
+		public string CreatedBy { get; set; }
+
+		public DateTime ValidatedOn { get; set; }
+		public string ValidatedBy { get; set; }
+	}
+}
