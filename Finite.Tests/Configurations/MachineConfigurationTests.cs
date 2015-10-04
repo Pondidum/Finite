ca423037aebc97d4e7609c92e47aa308e40e992f
andy <andy@andydote.co.uk>
2015-10-04 15:44:39 +0100

    machineConfiguration: allow state transitions to be specified by lambda directly

---
+++ Finite.Tests/Configurations/MachineConfigurationTests.cs
@@ -0,0 +1,181 @@
+﻿using Finite.Configurations;
+using Finite.Tests.TestData;
+using Shouldly;
+using Xunit;
+
+namespace Finite.Tests.Configurations
+{
+	public class MachineConfigurationTests
+	{
+		private readonly StateChangeEventArgs<TestArgs> _args;
+		private readonly MachineConfiguration<TestArgs> _config;
+
+		public MachineConfigurationTests()
+		{
+			_args = new StateChangeEventArgs<TestArgs>(new TestArgs(), new FirstState(), new SecondState());
+			_config = new MachineConfiguration<TestArgs>();
+		}
+
+		private void Reset()
+		{
+			_config.StateChangedHandler.OnResetState(this, _args);
+		}
+
+		private void Transition()
+		{
+			_config.StateChangedHandler.OnLeaveState(this, _args);
+			_config.StateChangedHandler.OnEnterState(this, _args);
+		}
+
+		[Fact]
+		public void When_specifying_nothing()
+		{
+			_config.OnStateChange();
+
+			Should.NotThrow(() => Reset());
+			Should.NotThrow(() => Transition());
+		}
+
+		[Fact]
+		public void When_specifying_on_enter()
+		{
+			var calls = 0;
+			_config.OnStateChange(enter: (s, e) => calls++);
+
+			Transition();
+			
+			calls.ShouldBe(1);
+		}
+
+		[Fact]
+		public void When_specifying_on_leave()
+		{
+			var calls = 0;
+			_config.OnStateChange(leave: (s, e) => calls++);
+
+			Transition();
+
+			calls.ShouldBe(1);
+		}
+
+		[Fact]
+		public void When_specifying_on_reset()
+		{
+			var calls = 0;
+			_config.OnStateChange(reset: (s, e) => calls++);
+
+			Reset();
+
+			calls.ShouldBe(1);
+		}
+
+		[Fact]
+		public void When_specifying_on_enter_twice()
+		{
+			var first = 0;
+			var second = 0;
+
+			_config.OnStateChange(enter: (s, e) => first++);
+			_config.OnStateChange(enter: (s, e) => second++);
+
+			Transition();
+
+			first.ShouldBe(0);
+			second.ShouldBe(1);
+		}
+
+		[Fact]
+		public void When_specifying_on_leave_twice()
+		{
+			var first = 0;
+			var second = 0;
+
+			_config.OnStateChange(leave: (s, e) => first++);
+			_config.OnStateChange(leave: (s, e) => second++);
+
+			Transition();
+
+			first.ShouldBe(0);
+			second.ShouldBe(1);
+		}
+
+		[Fact]
+		public void When_specifying_on_reset_twice()
+		{
+			var first = 0;
+			var second = 0;
+
+			_config.OnStateChange(reset: (s, e) => first++);
+			_config.OnStateChange(reset: (s, e) => second++);
+
+			Reset();
+
+			first.ShouldBe(0);
+			second.ShouldBe(1);
+		}
+
+		[Fact]
+		public void When_specifying_on_enter_and_on_leave_together()
+		{
+			var leave = 0;
+			var enter = 0;
+
+			_config.OnStateChange(
+				enter: (s, e) => enter++,
+				leave: (s, e) => leave++
+			);
+
+			Transition();
+
+			leave.ShouldBe(1);
+			enter.ShouldBe(1);
+		}
+
+		[Fact]
+		public void When_specifying_on_enter_and_on_leave_separately()
+		{
+			var leave = 0;
+			var enter = 0;
+
+			_config.OnStateChange(enter: (s, e) => enter++);
+			_config.OnStateChange(leave: (s, e) => leave++);
+
+			Transition();
+
+			leave.ShouldBe(1);
+			enter.ShouldBe(1);
+		}
+
+		[Fact]
+		public void When_there_is_already_a_custom_handler()
+		{
+			var enter = 0;
+
+			_config.OnStateChange(new ThrowingStateChangedHandler());
+			_config.OnStateChange(enter: (s, e) => enter++);
+
+			Transition();
+
+			enter.ShouldBe(1);
+		}
+
+
+		private class ThrowingStateChangedHandler: IStateChangedHandler<TestArgs>
+		{
+			public void OnLeaveState(object sender, StateChangeEventArgs<TestArgs> stateChangeArgs)
+			{
+				throw new System.NotImplementedException();
+			}
+
+			public void OnEnterState(object sender, StateChangeEventArgs<TestArgs> stateChangeArgs)
+			{
+				throw new System.NotImplementedException();
+			}
+
+			public void OnResetState(object sender, StateChangeEventArgs<TestArgs> stateChangeArgs)
+			{
+				throw new System.NotImplementedException();
+			}
+		}
+	}
+}
--- Finite/Configurations/MachineConfiguration.cs
+++ Finite/Configurations/MachineConfiguration.cs
@@ -1,4 +1,6 @@
-﻿namespace Finite.Configurations
+﻿using System;
+
+namespace Finite.Configurations
 {
 	public class MachineConfiguration<TSwitches>
 	{
@@ -11,6 +13,23 @@
 
 		public MachineConfiguration<TSwitches> OnStateChange(IStateChangedHandler<TSwitches> handler)
 		{
+			StateChangedHandler = handler;
+			return this;
+		}
+
+		public MachineConfiguration<TSwitches> OnStateChange(Action<object, StateChangeEventArgs<TSwitches>> enter = null, Action<object, StateChangeEventArgs<TSwitches>> leave = null, Action<object, StateChangeEventArgs<TSwitches>> reset = null)
+		{
+			var handler = StateChangedHandler as LambdaStateChangedHandler<TSwitches> ?? new LambdaStateChangedHandler<TSwitches>();
+
+			if (enter != null)
+				handler.OnEnter = enter;
+
+			if (leave != null)
+				handler.OnLeave = leave;
+
+			if (reset != null)
+				handler.OnReset = reset;
+
 			StateChangedHandler = handler;
 			return this;
 		}
