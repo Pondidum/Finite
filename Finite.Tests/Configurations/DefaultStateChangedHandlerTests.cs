8b459537c0a311e0583dc2abb9e0760106bd5025
andy <andy@andydote.co.uk>
2015-10-04 15:17:35 +0100

    defaultStateChangedHandler: add lambda expressions for configuring what happens on call

---
+++ Finite.Tests/Configurations/DefaultStateChangedHandlerTests.cs
@@ -0,0 +1,85 @@
+﻿using Finite.Configurations;
+using Finite.StateProviders;
+using Finite.Tests.TestData;
+using Shouldly;
+using Xunit;
+
+namespace Finite.Tests.Configurations
+{
+	public class DefaultStateChangedHandlerTests
+	{
+		private readonly StateMachine<TestArgs> _machine;
+		private MachineConfiguration<TestArgs> _config;
+
+		public DefaultStateChangedHandlerTests()
+		{
+			_config = new MachineConfiguration<TestArgs>();
+
+			var args = new TestArgs();
+			var stateProvider = new ScanningStateProvider<TestArgs>();
+
+			_machine = new StateMachine<TestArgs>(_config, stateProvider, args);
+		}
+
+		[Fact]
+		public void When_reseting_state_and_no_action_is_specified()
+		{
+			var handler = new DefaultStateChangedHandler<TestArgs>();
+			_config.OnStateChange(handler);
+
+			_machine.ResetTo<FirstState>();
+		}
+
+		[Fact]
+		public void When_changing_state_and_no_aciton_is_specified()
+		{
+			var handler = new DefaultStateChangedHandler<TestArgs>();
+			_config.OnStateChange(handler);
+
+			_machine.ResetTo<FirstState>();
+			_machine.TransitionTo<SecondState>();
+		}
+
+		[Fact]
+		public void When_reseting_state_and_there_is_an_action_specified()
+		{
+			var resetCalled = 0;
+			var handler = new DefaultStateChangedHandler<TestArgs>();
+			handler.OnReset = (s, a) => { resetCalled++; };
+
+			_config.OnStateChange(handler);
+
+			_machine.ResetTo<FirstState>();
+
+			resetCalled.ShouldBe(1);
+		}
+
+		[Fact]
+		public void When_changing_state_and_an_onleave_action_is_specified()
+		{
+			var leaveCalled = 0;
+			var handler = new DefaultStateChangedHandler<TestArgs>();
+			handler.OnLeave = (s, a) => { leaveCalled++; };
+
+			_config.OnStateChange(handler);
+			_machine.ResetTo<FirstState>();
+
+			_machine.TransitionTo<SecondState>();
+			leaveCalled.ShouldBe(1);
+		}
+
+		[Fact]
+		public void When_changing_state_and_an_onenter_action_is_specified()
+		{
+			var enterCalled = 0;
+			var handler = new DefaultStateChangedHandler<TestArgs>();
+			handler.OnEnter = (s, a) => { enterCalled++; };
+
+			_config.OnStateChange(handler);
+			_machine.ResetTo<FirstState>();
+
+			_machine.TransitionTo<SecondState>();
+			enterCalled.ShouldBe(1);
+		}
+	}
+}
--- Finite/Configurations/DefaultStateChangedHandler.cs
+++ Finite/Configurations/DefaultStateChangedHandler.cs
@@ -1,17 +1,26 @@
-﻿namespace Finite.Configurations
+﻿using System;
+
+namespace Finite.Configurations
 {
 	public class DefaultStateChangedHandler<TSwitches> : IStateChangedHandler<TSwitches>
 	{
+		public Action<object, StateChangeEventArgs<TSwitches>> OnLeave { get; set; }
+		public Action<object, StateChangeEventArgs<TSwitches>> OnEnter { get; set; }
+		public Action<object, StateChangeEventArgs<TSwitches>> OnReset { get; set; }
+
 		public void OnLeaveState(object sender, StateChangeEventArgs<TSwitches> stateChangeArgs)
 		{
+			OnLeave?.Invoke(sender, stateChangeArgs);
 		}
 
 		public void OnEnterState(object sender, StateChangeEventArgs<TSwitches> stateChangeArgs)
 		{
+			OnEnter?.Invoke(sender, stateChangeArgs);
 		}
 
 		public void OnResetState(object sender, StateChangeEventArgs<TSwitches> stateChangeArgs)
 		{
+			OnReset?.Invoke(sender, stateChangeArgs);
 		}
 	}
 }
