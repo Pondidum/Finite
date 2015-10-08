cf2c03fefdff52133244d6027755afe6ae3a16bb
andy <andy@andydote.co.uk>
2015-10-08 18:28:52 +0100

    samples: add sample.rendering project

---
+++ Sample.Rendering/GraphicalOutput.cs
@@ -0,0 +1,34 @@
+﻿using Finite;
+using Finite.Renderers;
+using Finite.StateProviders;
+using Sample.Common;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Sample.Rendering
+{
+	public class GraphicalOutput
+	{
+		private readonly ITestOutputHelper _output;
+
+		public GraphicalOutput(ITestOutputHelper output)
+		{
+			_output = output;
+		}
+
+		[Fact]
+		public void Rendering_the_state_machine_with_graphviz()
+		{
+			var machine = new StateMachine<CreditRequest>(
+				new ScanningStateProvider<CreditRequest>(),
+				new CreditRequest());
+
+			var renderer = new GraphvizRenderer();
+			renderer.Render(machine);
+
+			_output.WriteLine("Copy and paste the following output into http://www.webgraphviz.com/ :");
+
+			_output.WriteLine(renderer.Output);
+		}
+	}
+}
